5fd380ce1bb44b94f908794fcd802f0f83bfbf51
saboalfazl6162 <saboalfazl6162@gmail.com>
2025-11-11 15:26:52 +0330

    debug code

--- apps/qa/views.py
+++ apps/qa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,CreateView
 from .models import Question,Answer
 
 class QaMainView(TemplateView):
@@ -9,3 +9,10 @@ class QaMainView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['questions'] = Question.objects.filter(is_active = True)
         return context
+
+class QuestionCreateView(CreateView):
+    model = Question
+    template_name = "question_create.html"
+    def form_valid(self, form):
+        cleaned_data = form.cleaned_data
+        return super().form_valid(form)
